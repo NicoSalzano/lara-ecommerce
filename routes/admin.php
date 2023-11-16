644e4718e128dc9a7a0008ff53083d05f9941bc3
Nicola Salzano <nicolasalzano2913@gmail.com>
2023-11-16 18:48:08 +0100

    add file for route e canche the providers

---
+++ routes/admin.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\AdminController;
+
+// Admin routes 
+Route::get('dashboard',[AdminController::class, 'dashboard'])->name('dashboard');
